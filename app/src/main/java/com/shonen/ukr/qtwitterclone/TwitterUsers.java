60f42034219d5fa9ab1e497089c0ec828eb0280a
Dima Usatyuk <Shonen1988@gmail.com>
2020-05-01 17:02:57 +0300

    add toolbar and menu options

--- app/src/main/java/com/shonen/ukr/qtwitterclone/TwitterUsers.java
+++ app/src/main/java/com/shonen/ukr/qtwitterclone/TwitterUsers.java
@@ -1,20 +1,48 @@
 package com.shonen.ukr.qtwitterclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
+
+import com.parse.ParseUser;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class TwitterUsers extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter_users);
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
 
-        FancyToast.makeText(TwitterUsers.this,"Welcome!", Toast.LENGTH_SHORT,FancyToast.INFO,false).show();
 
+        FancyToast.makeText(TwitterUsers.this, "Welcome!", Toast.LENGTH_SHORT, FancyToast.INFO, false).show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.logOutIten){
+            ParseUser.getCurrentUser().logOut();
+            Intent intent = new Intent(TwitterUsers.this,LogIn.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
