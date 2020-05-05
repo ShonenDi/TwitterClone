cfbb29e2de6c435e1d52eb47b950c53389f22921
Dima Usatyuk <Shonen1988@gmail.com>
2020-05-05 14:28:59 +0300

    fix list view and menu

--- app/src/main/java/com/shonen/ukr/qtwitterclone/TwitterUsers.java
+++ app/src/main/java/com/shonen/ukr/qtwitterclone/TwitterUsers.java
@@ -6,35 +6,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TwitterUsers extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private ListView listView;
+    private ArrayList<String> usersArr;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter_users);
         setTitle("Twitter users");
-        toolbar = findViewById(R.id.myToolbar);
-        setSupportActionBar(toolbar);
+
+        listView = findViewById(R.id.idListView);
+        usersArr = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,usersArr);
 
 
-        FancyToast.makeText(TwitterUsers.this, "Welcome!", Toast.LENGTH_SHORT, FancyToast.INFO, false).show();
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e==null){
+                    if(objects.size()>0){
+                        for(ParseUser user:objects){
+                            usersArr.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);
+                    }
+                }
+            }
+        });
+
+        FancyToast.makeText(TwitterUsers.this, "Welcome " + ParseUser.getCurrentUser().getUsername(), Toast.LENGTH_SHORT, FancyToast.INFO, false).show();
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.my_menu,menu);
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.my_menu,menu);
+        return true;
     }
 
     @Override
@@ -42,7 +73,7 @@ public class TwitterUsers extends AppCompatActivity {
         if(item.getItemId()==R.id.logOutItem){
             ParseUser.getCurrentUser().logOut();
             finish();
-            Intent intent = new Intent(TwitterUsers.this,LogIn.class);
+            Intent intent = new Intent(TwitterUsers.this,SingUp.class);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
